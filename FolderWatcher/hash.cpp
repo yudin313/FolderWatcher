c2c8c390d74b72a876817191e9f6852158ba7407
Papa-Pedro-13 <62937441+Papa-Pedro-13@users.noreply.github.com>
2023-04-15 23:36:47 +0300

    Хэш для 2го ТЗ

--- FolderWatcher/hash.cpp
+++ FolderWatcher/hash.cpp
@@ -1,6 +1,31 @@
 #include "hash.h"
-
 using namespace std;
+//helper function to print the digest bytes as a hex string
+string bytes_to_hex_string(const vector<uint8_t>& bytes)
+{
+    ostringstream stream;
+    for (uint8_t b : bytes)
+    {
+        stream << setw(2) << setfill('0') << hex << static_cast<int>(b);
+    }
+    return stream.str();
+}
+
+//perform the SHA3-512 hash
+string sha3_512(const string& input)
+{
+    uint32_t digest_length = SHA512_DIGEST_LENGTH;
+    const EVP_MD* algorithm = EVP_sha3_512();
+    uint8_t* digest = static_cast<uint8_t*>(OPENSSL_malloc(digest_length));
+    EVP_MD_CTX* context = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(context, algorithm, nullptr);
+    EVP_DigestUpdate(context, input.c_str(), input.size());
+    EVP_DigestFinal_ex(context, digest, &digest_length);
+    EVP_MD_CTX_destroy(context);
+    string output = bytes_to_hex_string(vector<uint8_t>(digest, digest + digest_length));
+    OPENSSL_free(digest);
+    return output;
+}
 
 inline string SHA256(char* path)
 {
@@ -38,7 +63,7 @@ inline string SHA256(char* path)
     return os.str();
 }
 
-inline string MD5(char* path)
+inline string SHA512(char* path)
 {
     ifstream fp(path, ios::in | ios::binary);
 
@@ -51,37 +76,97 @@ inline string MD5(char* path)
     constexpr const size_t buffer_size{ 1 << 12 };
     char buffer[buffer_size];
 
-    unsigned char hash[MD5_DIGEST_LENGTH] = { 0 };
+    unsigned char hash[SHA512_DIGEST_LENGTH] = { 0 };
 
-    MD5state_st ctx;
-    MD5_Init(&ctx);
+    SHA512state_st ctx;
+    SHA512_Init(&ctx);
 
     while (fp.good()) {
         fp.read(buffer, buffer_size);
-        MD5_Update(&ctx, buffer, (size_t)fp.gcount());
+        SHA512_Update(&ctx, buffer, (size_t)fp.gcount());
     }
 
-    MD5_Final(hash, &ctx);
+    SHA512_Final(hash, &ctx);
     fp.close();
 
     ostringstream os;
     os << hex << setfill('0');
 
-    for (int i = 0; i < MD5_DIGEST_LENGTH; ++i) {
+    for (int i = 0; i < SHA512_DIGEST_LENGTH; ++i) {
         os << setw(2) << static_cast<unsigned int>(hash[i]);
     }
 
     return os.str();
 }
 
-int hashes_file(char* name_path, char* hash_path)
+inline string SHA384(char* path)
+{
+    ifstream fp(path, ios::in | ios::binary);
+
+    if (!fp.good()) {
+        ostringstream os;
+        os << "Cannot open \"" << path << "\": " << strerror(errno) << ".";
+        throw runtime_error(os.str());
+    }
+
+    constexpr const size_t buffer_size{ 1 << 12 };
+    char buffer[buffer_size];
+
+    unsigned char hash[SHA384_DIGEST_LENGTH] = { 0 };
+
+    SHA512state_st ctx;
+    SHA384_Init(&ctx);
+
+    while (fp.good()) {
+        fp.read(buffer, buffer_size);
+        SHA384_Update(&ctx, buffer, (size_t)fp.gcount());
+    }
+
+    SHA384_Final(hash, &ctx);
+    fp.close();
+
+    ostringstream os;
+    os << hex << setfill('0');
+
+    for (int i = 0; i < SHA384_DIGEST_LENGTH; ++i) {
+        os << setw(2) << static_cast<unsigned int>(hash[i]);
+    }
+
+    return os.str();
+}
+
+bool readfile(std::string& s, const char* filename) {
+    std::ifstream fp(filename);
+    if (!fp.is_open())
+        return false;
+
+    char nil = '\0';
+    std::getline(fp, s, nil);
+    fp.close();
+    return (s.length() > 0);
+}
+
+int hashes_file(char* cast_path, char* hash_path, char* file_path)
 {
     ofstream fout;
     fout.open(hash_path, ios::out);
-
+    std::string s;
+    readfile(s, cast_path);
     try {
-        fout << "SHA256:" << SHA256(name_path) << endl;
-        fout << "MD5:" << MD5(name_path) << endl;
+        fout << "SHA256-snapshot:" << SHA256(cast_path) << endl;
+        fout << "SHA512-snapshot:" << SHA512(cast_path) << endl;
+        std::string output = sha3_512(s);
+        fout << "SHA3-512-snapshot:" << output << endl;
+    }
+    catch (const exception& e) {
+        cerr << "[fatal] " << e.what() << endl;
+    }
+    readfile(s, file_path);
+    try {
+        fout << "SHA256-file:" << SHA256(file_path) << endl;
+        fout << "SHA512-file:" << SHA512(file_path) << endl;
+        std::string output = sha3_512(s);
+        fout << "SHA3-512-file:" << output << endl;
     }
     catch (const exception& e) {
         cerr << "[fatal] " << e.what() << endl;
--- FolderWatcher/hash.h
+++ FolderWatcher/hash.h
@@ -1,11 +1,13 @@
 #pragma once
-
+#pragma warning(disable : 4996)
 #include <fstream>
 #include <iostream>
-#include <iomanip>
-#include <sstream>
+#include <vector>
+#include <sstream> //for std::ostringstream
+#include <iomanip> //for std::setw, std::hex, and std::setfill
 
+#include <openssl/evp.h> //for all other OpenSSL function calls
 #include <openssl/sha.h>
 #include <openssl/md5.h>
 
-int hashes_file(char*, char*);
+int hashes_file(char*, char*,char*);
